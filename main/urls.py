e0e681d6fc1a3a2ea183f8a4f83b9ec6e5e61484
Adretanoo <adriantegzza@gmail.com>
2025-07-30 13:58:56 +0300

    create publication pages

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,17 @@ from django.urls import path
 from main import views
 from django.conf.urls.i18n import i18n_patterns
 
-
 urlpatterns = [
-    path('',views.main,name='main'),
-]
+    path('', views.main, name='main'),
+    path('about/', views.about_page, name='about_page'),
+    path('cafe-bar/', views.cafe_bar_page, name='cafe_bar_page'),
+    path('vip-hall/', views.vip_hall_page, name='vip_hall_page'),
+    path('advertising/', views.advertising_page, name='advertising_page'),
+    path('children-room/', views.children_room_page, name='children_room_page'),
 
+    path('shares/', views.shares_page, name='shares_page'),
+    path('news/', views.news_page, name='news_page'),
+
+    path('shares/card/<int:pk>/',views.shares_card, name='shares_card'),
+    path('news/card/<int:pk>/',views.news_card, name='news_card'),
+]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
-
 from datetime import date
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+
 from adminlte.models import MainPage, TopBanner, TopBannerImage, BackgroundBanner, BackgroundType, Movie, NewsBanner, \
-    NewsBannerImage, Publication, PublicationType
+    NewsBannerImage, Publication, PublicationType, PublicationGallery
 
 
 def main(request):
@@ -20,18 +22,153 @@ def main(request):
     news_pages = Publication.objects.filter(publication_type=PublicationType.NEW_PAGE)
     print(news_pages)
 
-
-
     context = {
         'main_page': main_page,
         'top_banner': top_banner,
         'top_banner_images': top_banner_images,
         'bg_banner': bg_banner,
-        'bg_type':BackgroundType,
+        'bg_type': BackgroundType,
         'movie_today': movie_today,
         'movie_soon': movie_soon,
         'news_banner': news_banner,
         'news_banner_images': news_banner_images,
         'news_pages': news_pages,
     }
-    return render(request, 'main/page/index.html',context)
+    return render(request, 'main/page/index.html', context)
+
+
+def shares_page(request):
+    seo_page = [{
+        'title': 'Акции',
+        'keywords': 'dsfja',
+        'description': 'text'
+    }]
+    return shares_news_page(request, PublicationType.SHARES, seo_page)
+
+
+def news_page(request):
+    seo_page = [{
+        'title': 'Новини',
+        'keywords': 'dsfja',
+        'description': 'text'
+    }]
+    return shares_news_page(request, PublicationType.NEWS, seo_page)
+
+
+def about_page(request):
+    return publication_page(request, PublicationType.ABOUT)
+
+
+def cafe_bar_page(request):
+    return publication_page(request, PublicationType.CAFE_BAR)
+
+
+def vip_hall_page(request):
+    return publication_page(request, PublicationType.VIP_HALL)
+
+
+def advertising_page(request):
+    return publication_page(request, PublicationType.ADVERTISING)
+
+
+def children_room_page(request):
+    return publication_page(request, PublicationType.CHILDREN_ROOM)
+
+
+def shares_news_page(request, publication_type, seo_global_page):
+
+    pub_list = Publication.objects.filter(publication_type=publication_type, is_enabled=True)
+
+    top_banner = TopBanner.objects.first()
+    top_banner_images = TopBannerImage.objects.all()
+
+    paginator = Paginator(pub_list, 3)
+    page = request.GET.get('page')
+
+    try:
+        pub = paginator.page(page)
+    except PageNotAnInteger:
+        pub = paginator.page(1)
+    except EmptyPage:
+        pub = paginator.page(paginator.num_pages)
+
+    context = {
+        'pub': pub,
+        'seo_global_page': seo_global_page,
+        'top_banner': top_banner,
+        'top_banner_images': top_banner_images,
+        'pub_type': PublicationType,
+        'publication_type': publication_type,
+    }
+    return render(request, 'main/page/base_shares_news.html', context)
+
+
+def shares_card(request, pk):
+    return shares_news_card(request, PublicationType.SHARES, pk)
+
+
+def news_card(request, pk):
+    return shares_news_card(request, PublicationType.NEWS, pk)
+
+
+def shares_news_card(request, publication_type, pk):
+    try:
+        pub = Publication.objects.get(pk=pk)
+    except Publication.DoesNotExist:
+        pub = None
+
+    gallery = PublicationGallery.objects.filter(publication=pub).select_related('image')
+
+    top_banner = TopBanner.objects.first()
+    top_banner_images = TopBannerImage.objects.all()
+
+    video_html = f'''
+            <div class="video-wrapper my-4">
+                    <iframe width="100%" height="515"
+                        src="{pub.video_url}"
+                        title="Відео"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen>
+                    </iframe>
+                </div>
+            '''
+    desc_with_breaks = pub.description.replace('\n', '<br>')
+    desc_format = desc_with_breaks.replace('[VIDEO]', video_html)
+
+    context = {
+        'pub': pub,
+        'top_banner': top_banner,
+        'top_banner_images': top_banner_images,
+        'pub_type': PublicationType,
+        'publication_type': publication_type,
+        'desc_with_video': desc_format,
+        'gallery': gallery,
+    }
+    return render(request, 'main/page/base_shares_news_card.html', context)
+
+
+def publication_page(request, publication_type):
+    pub = Publication.objects.get(publication_type=publication_type)
+    gallery = PublicationGallery.objects.filter(publication=pub).select_related('image')
+
+    video_html = f'''
+        <div class="video-wrapper my-4">
+                <iframe width="100%" height="515"
+                    src="{pub.video_url}"
+                    title="Відео"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+                </iframe>
+            </div>
+        '''
+
+    desc_with_breaks = pub.description.replace('\n', '<br>')
+    desc_format = desc_with_breaks.replace('[VIDEO]', video_html)
+    context = {
+        'publication': pub,
+        'desc_with_video': desc_format,
+        'gallery': gallery,
+    }
+    return render(request, 'main/page/base_publication.html', context)
