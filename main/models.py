dfc2f48b3082f359778daf007d27114d2cc02314
Adretanoo <adriantegzza@gmail.com>
2025-08-02 00:40:07 +0300

    create tables main

--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from adminlte.models import Movie, CardHall
+from user.models import User
+
+
+class StatusPayment(models.TextChoices):
+    BOUGHT = 'bought','Куплено'
+    BLOCKED = 'blocked','Заброньовано'
+
+
+class Session(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    card_hall = models.ForeignKey(CardHall, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.movie} - {self.card_hall}'
+
+
+class Seat(models.Model):
+    hall = models.ForeignKey(CardHall, on_delete=models.CASCADE)
+    row = models.SmallIntegerField()
+    column = models.SmallIntegerField()
+
+    def __str__(self):
+        return f'{self.hall} - {self.row} - {self.column}'
+
+
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=StatusPayment.choices, max_length=10)
+
+    def __str__(self):
+        return f'{self.user} - {self.session} - {self.created_at}'
